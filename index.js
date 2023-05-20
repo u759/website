0b33e140936ba9e875aac6459d365563288b36b2
ahnlive <aydennguyen6042@gmail.com>
2023-05-19 17:44:40 -0700

    photography and fade in lazy

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-        console.log(entry)
         if (entry.isIntersecting) {
             entry.target.classList.add('show');
         }
@@ -13,3 +12,36 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+/*window.onload = () => {
+    const transition_el = document.querySelector('.transition');
+    const anchors = document.querySelectorAll('a'); //anchors are links on page
+    setTimeout(() => {
+        transition_el.classList.remove('is-active');
+    }, 500);
+
+    for (let i = 0; i < anchors.length; i++) {
+
+        const anchor = anchors[i];
+        anchor.addEventListener('click', e => {
+
+            let target = e.target.href;
+            console.log(target);
+
+
+            e.preventDefault();
+            transition_el.classList.add('is-active');
+            setTimeout(() => {
+                window.location.href = target;
+            }, 500);
+        });
+    }
+
+}*/
+
+[].forEach.call(document.querySelectorAll('img[data-src]'), function(img) {
+	img.setAttribute('src', img.getAttribute('data-src'));
+	img.onload = function() {
+		img.removeAttribute('data-src');
+	};
+}); //please understand how this works
