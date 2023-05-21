996939cf8e8fc2fecd36cb006230253d752c56ae
ahnlive <aydennguyen6042@gmail.com>
2023-05-20 18:11:44 -0700

    experience, notables

--- index.js
+++ index.js
@@ -13,32 +13,6 @@ const observer = new IntersectionObserver((entries) => {
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
 
-/*window.onload = () => {
-    const transition_el = document.querySelector('.transition');
-    const anchors = document.querySelectorAll('a'); //anchors are links on page
-    setTimeout(() => {
-        transition_el.classList.remove('is-active');
-    }, 500);
-
-    for (let i = 0; i < anchors.length; i++) {
-
-        const anchor = anchors[i];
-        anchor.addEventListener('click', e => {
-
-            let target = e.target.href;
-            console.log(target);
-
-
-            e.preventDefault();
-            transition_el.classList.add('is-active');
-            setTimeout(() => {
-                window.location.href = target;
-            }, 500);
-        });
-    }
-
-}*/
-
 [].forEach.call(document.querySelectorAll('img[data-src]'), function(img) {
 	img.setAttribute('src', img.getAttribute('data-src'));
 	img.onload = function() {
